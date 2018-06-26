6afc078a9f95f15f2fab7eca3050b7db3dc35dd0
DZbug <dzbugeek@gmail.com>
2018-06-26 20:10:36 +0300

    Ошибка

--- chapter7/admin/includes/db.inc.php
+++ chapter7/admin/includes/db.inc.php
@@ -5,9 +5,9 @@ try
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$pdo->exec('SET NAMES "utf8"');
 }
-catch (PDOExeption $e)
+catch (PDOException $e)
 {
-	$error = 'не удалось подключится к серверу баз данных.';
+	$error = 'Не удалось подключится к серверу баз данных.';
 	include 'error.html.php';
 	exit();
 }
